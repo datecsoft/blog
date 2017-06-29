f6dd6447ac20037b4a9d45cb3c692dccb3c2d85b
datecsoft <datecsoft@gmail.com>
2017-06-28 21:06:00 -0500

    REUPLOAD

---
+++ js/SmoothScroll.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.menu-item').click(function(e){
+        
+        var linkhref = $(this).find("a").attr("href");
+        
+        $('html, body').stop().animate({
+        	scrollTop:$(linkhref).offset().top
+        	        }, 1500, 'easeInOutExpo');
+
+        e.preventDefault();
+
+    });
+});
---
+++ js/scripts.js
@@ -0,0 +1,141 @@
+// ------------------------------------------------------------------------
+// Check if element is in ViewPort
+// ------------------------------------------------------------------------
+(function($) {
+    $.belowthefold = function(element, settings) {
+        var fold = $(window).height() + $(window).scrollTop();
+        return fold <= $(element).offset().top - settings.threshold;
+    };
+    $.abovethetop = function(element, settings) {
+        var top = $(window).scrollTop();
+        return top >= $(element).offset().top + $(element).height() - settings.threshold;
+    };
+    $.rightofscreen = function(element, settings) {
+        var fold = $(window).width() + $(window).scrollLeft();
+        return fold <= $(element).offset().left - settings.threshold;
+    };
+    $.leftofscreen = function(element, settings) {
+        var left = $(window).scrollLeft();
+        return left >= $(element).offset().left + $(element).width() - settings.threshold;
+    };
+    $.inviewport = function(element, settings) {
+        return !$.rightofscreen(element, settings) && !$.leftofscreen(element, settings) && !$.belowthefold(element, settings) && !$.abovethetop(element, settings);
+    };
+    $.extend($.expr[':'], {
+        "below-the-fold": function(a, i, m) {
+            return $.belowthefold(a, {
+                threshold: 0
+            });
+        },
+        "above-the-top": function(a, i, m) {
+            return $.abovethetop(a, {
+                threshold: 0
+            });
+        },
+        "left-of-screen": function(a, i, m) {
+            return $.leftofscreen(a, {
+                threshold: 0
+            });
+        },
+        "right-of-screen": function(a, i, m) {
+            return $.rightofscreen(a, {
+                threshold: 0
+            });
+        },
+        "in-viewport": function(a, i, m) {
+            return $.inviewport(a, {
+                threshold: 0
+            });
+        }
+    });
+    // ------------------------------------------------------------------------
+    // Classie Script
+    // ------------------------------------------------------------------------
+    (function(window) {
+        'use strict';
+        function classReg(className) {
+            return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+        }
+        var hasClass, addClass, removeClass;
+        if ('classList' in document.documentElement) {
+            hasClass = function(elem, c) {
+                return elem.classList.contains(c);
+            };
+            addClass = function(elem, c) {
+                elem.classList.add(c);
+            };
+            removeClass = function(elem, c) {
+                elem.classList.remove(c);
+            };
+        } else {
+            hasClass = function(elem, c) {
+                return classReg(c).test(elem.className);
+            };
+            addClass = function(elem, c) {
+                if (!hasClass(elem, c)) {
+                    elem.className = elem.className + ' ' + c;
+                }
+            };
+            removeClass = function(elem, c) {
+                elem.className = elem.className.replace(classReg(c), ' ');
+            };
+        }
+        function toggleClass(elem, c) {
+            var fn = hasClass(elem, c) ? removeClass : addClass;
+            fn(elem, c);
+        }
+        var classie = {
+            hasClass: hasClass,
+            addClass: addClass,
+            removeClass: removeClass,
+            toggleClass: toggleClass,
+            has: hasClass,
+            add: addClass,
+            remove: removeClass,
+            toggle: toggleClass
+        };
+        if (typeof define === 'function' && define.amd) {
+            define(classie);
+        } else {
+            window.classie = classie;
+        }
+
+    })(window);
+    // ------------------------------------------------------------------------
+    // Animated Header
+    // ------------------------------------------------------------------------
+
+    var cbpAnimatedHeader = (function() {
+        var docElem = document.documentElement,
+            header = document.querySelector('.navbar-default'),
+            didScroll = false,
+            changeHeaderOn = 50;
+        function init() {
+            window.addEventListener('scroll', function(event) {
+                if (!didScroll) {
+                    didScroll = true;
+                    setTimeout(scrollPage, 100);
+                }
+            }, false);
+            window.addEventListener('load', function(event) {
+                if (!didScroll) {
+                    didScroll = true;
+                    setTimeout(scrollPage, 100);
+                }
+            }, false);
+        }
+        function scrollPage() {
+            var sy = scrollY();
+            if (sy >= changeHeaderOn) {
+                classie.add(header, 'navbar-shrink');
+            } else {
+                classie.remove(header, 'navbar-shrink');
+            }
+            didScroll = false;
+        }
+        function scrollY() {
+            return window.pageYOffset || docElem.scrollTop;
+        }
+        init();
+    })();
+})(jQuery);
